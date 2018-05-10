df38ca028cc5dbd79e040b380360819047731598
Emlinn <emilragnarsson@MacbookPro-2.local>
2018-05-10 09:51:53 +0000

    gaman saman
    gaman

--- Dev/fjarmalx/src/fjarmal/forms.py
+++ Dev/fjarmalx/src/fjarmal/forms.py
@@ -5,8 +5,12 @@ STRAT_CHOICES= [
     ('strat1', 'Strategy 1'),
     ('strat2', 'Strategy 2'),
     ('strat3', 'Strategy 3'),
+<<<<<<< Updated upstream
     ('comp', 'Strat. Compare')
     ]
+=======
+]
+>>>>>>> Stashed changes
 
 DATE_CHOICES = [
     (10, '10 days'),
@@ -21,6 +25,6 @@ class RiskFreeRateForm(forms.Form):
 class StratForm(forms.Form):
     comission = forms.FloatField(label='Comission:', min_value=0, max_value=1)
     capital =  forms.IntegerField(label=mark_safe('Capital '), min_value=0, max_value=100000000)
-    rate = forms.FloatField(label=mark_safe('Risk Free Rate'), min_value=0, max_value=1)
+    rate = forms.FloatField(label=mark_safe('Risk Free Rate'), min_value=0, max_value=100)
     pick_strat = forms.CharField(label=mark_safe('Strategy '),widget=forms.Select(choices=STRAT_CHOICES))
     pick_date = forms.CharField(label=mark_safe('Update Interval '),widget=forms.Select(choices=DATE_CHOICES))
